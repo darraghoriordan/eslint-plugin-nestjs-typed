bce5f175b73b0f231da839b6d68f674acb749a3a
Darragh ORiordan <darragh.oriordan@gmail.com>
2021-11-06 17:03:40 +1100

    fix: the documentation path should point to the readme

--- src/utils/createRule.ts
+++ src/utils/createRule.ts
@@ -2,11 +2,13 @@ import {ESLintUtils} from "@typescript-eslint/experimental-utils";
 
 // Note - cannot migrate this to an import statement because it will make TSC copy the package.json to the dist folder
 // eslint-disable-next-line @typescript-eslint/no-var-requires, unicorn/prefer-module
-const {version} = require("../../package.json");
+//const {version} = require("../../package.json");
 // eslint-disable-next-line new-cap
 export const createRule = ESLintUtils.RuleCreator(
-    (name) =>
-        `https://github.com/darraghoriordan/eslint-plugin-nestjs/blob/v${
-            version as string
-        }/packages/eslint-plugin/docs/rules/${name}.md`
+    // (name) =>
+    //     `https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/v${
+    //         version as string
+    //     }/packages/eslint-plugin/docs/rules/${name}.md`
+    () =>
+        `https://github.com/darraghoriordan/eslint-plugin-nestjs-typed/blob/main/README.md`
 );
