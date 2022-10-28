524d13cfcc4bfa787732ddf392f3a355fbbbd31e
Daniel <foo-bar-sketch@protonmail.com>
2022-10-28 11:44:08 +0200

    feat(allPropertiesAreWhitelisted): adds checking imported decorator is from class-validator

--- src/rules/allPropertiesAreWhitelisted/allPropertiesAreWhitelisted.ts
+++ src/rules/allPropertiesAreWhitelisted/allPropertiesAreWhitelisted.ts
@@ -1,8 +1,65 @@
 import {AST_NODE_TYPES, TSESTree, TSESLint} from "@typescript-eslint/utils";
 import {createRule} from "../../utils/createRule";
-import {classValidatorDecorators} from "../../utils/classValidatorDecorators";
 
-const CLASS_VALIDATOR_DECORATOR_NAMES = new Set(classValidatorDecorators);
+const importIsDecorator = (
+    imp: TSESTree.ImportDeclaration,
+    decoratorName: string
+): boolean => {
+    const isFromClassValidator = imp.source.value.startsWith("class-validator");
+    const isDecoratorImport = imp.specifiers.some(
+        (specifier) => specifier.local.name === decoratorName
+    );
+
+    return isFromClassValidator && isDecoratorImport;
+};
+
+const decoratorIsImportedFromClassValidator = (
+    imports: TSESTree.ImportDeclaration[],
+    decorator: TSESTree.Decorator
+): boolean => {
+    if (decorator.expression.type !== TSESTree.AST_NODE_TYPES.CallExpression) {
+        return false;
+    }
+
+    if (
+        decorator.expression.callee.type !== TSESTree.AST_NODE_TYPES.Identifier
+    ) {
+        return false;
+    }
+
+    const decoratorName = decorator.expression.callee.name;
+
+    return imports.some((imp) => importIsDecorator(imp, decoratorName));
+};
+
+const decoratorIsClassValidatorDecorator = (
+    program: TSESTree.Program | null,
+    decorator: TSESTree.Decorator
+): boolean => {
+    if (!program) {
+        return false;
+    }
+
+    const imports = program.body.filter(
+        (node) => node.type === TSESTree.AST_NODE_TYPES.ImportDeclaration
+    ) as TSESTree.ImportDeclaration[];
+
+    return decoratorIsImportedFromClassValidator(imports, decorator);
+};
+
+const getRootProgram = (node: TSESTree.BaseNode): TSESTree.Program | null => {
+    let root = node;
+
+    while (root.parent) {
+        if (root.parent.type === TSESTree.AST_NODE_TYPES.Program) {
+            return root.parent;
+        }
+
+        root = root.parent;
+    }
+
+    return null;
+};
 
 const rule = createRule({
     name: "all-properties-are-whitelisted",
@@ -29,6 +86,7 @@ const rule = createRule({
         return {
             // eslint-disable-next-line @typescript-eslint/naming-convention
             ClassDeclaration(node) {
+                const program = getRootProgram(node);
                 const withDecorator: TSESTree.PropertyDefinition[] = [];
                 const withoutDecorator: TSESTree.PropertyDefinition[] = [];
                 for (const element of node.body.body) {
@@ -41,8 +99,9 @@ const rule = createRule({
                                 AST_NODE_TYPES.CallExpression &&
                             decorator.expression.callee.type ===
                                 AST_NODE_TYPES.Identifier &&
-                            CLASS_VALIDATOR_DECORATOR_NAMES.has(
-                                decorator.expression.callee.name
+                            decoratorIsClassValidatorDecorator(
+                                program,
+                                decorator
                             )
                     );
                     if (hasDecorator) {
