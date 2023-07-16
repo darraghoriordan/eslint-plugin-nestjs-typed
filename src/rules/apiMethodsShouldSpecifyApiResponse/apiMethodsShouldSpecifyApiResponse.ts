9fff267618976af027a9153af68d529556c81994
Darragh ORiordan <darragh.oriordan@gmail.com>
2023-07-16 12:31:31 +1000

    chore: clean up old commented imports

--- src/rules/apiMethodsShouldSpecifyApiResponse/apiMethodsShouldSpecifyApiResponse.ts
+++ src/rules/apiMethodsShouldSpecifyApiResponse/apiMethodsShouldSpecifyApiResponse.ts
@@ -1,6 +1,3 @@
-// Import { getParserServices } from "@typescript-eslint/experimental-utils/dist/eslint-utils";
-// import * as tsutils from "tsutils";
-// import { getParserServices } from "@typescript-eslint/experimental-utils/dist/eslint-utils";
 import {TSESTree, TSESLint} from "@typescript-eslint/utils";
 import {createRule} from "../../utils/createRule";
 import {typedTokenHelpers} from "../../utils/typedTokenHelpers";
