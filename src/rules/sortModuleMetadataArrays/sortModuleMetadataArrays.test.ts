8af637f4879d356935d12d2fc34a6d497bbafe8d
Darragh ORiordan <darragh.oriordan@gmail.com>
2024-09-08 21:58:24 +1000

    fix: tests

--- src/rules/sortModuleMetadataArrays/sortModuleMetadataArrays.test.ts
+++ src/rules/sortModuleMetadataArrays/sortModuleMetadataArrays.test.ts
@@ -70,7 +70,8 @@ ruleTester.run("sortModuleMetadataArrays", rule, {
                 ],
             })
             export class MainModule {}`,
-            output: `@Module({
+            output: [
+                `@Module({
                 imports: [
                     CoreModule,
                     DatabaseModule,
@@ -95,125 +96,253 @@ ruleTester.run("sortModuleMetadataArrays", rule, {
                 ],
             })
             export class MainModule {}`,
+                `@Module({
+                imports: [
+                    CoreModule,
+                    DatabaseModule,
+                    AuthzModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+                `@Module({
+                imports: [
+                    CoreModule,
+                    AuthzModule,
+                    DatabaseModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+                `@Module({
+                imports: [
+                    AuthzModule,
+                    CoreModule,
+                    DatabaseModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+            ],
             errors: [
                 {
                     messageId: "moduleMetadataArraysAreSorted",
                 },
             ],
         },
-        // {
-        //     code: `@Module({
-        //         imports: [
-        //             CoreModule,
-        //             DatabaseModule,
-        //             OpenTelemetryModule.forRoot({
-        //                 metrics: {
-        //                     hostMetrics: true,
-        //                     apiMetrics: {
-        //                         enable: true,
-        //                     },
-        //                 },
-        //             }),
-        //             AuthzModule,
-        //             UserInternalModule,
-        //         ],
-        //         controllers: [
-        //             AController,
-        //             BController
-        //         ],
-        //         providers: [
-        //             AProvider,
-        //             BProvider
-        //         ],
-        //     })
-        //     export class MainModule {}`,
-        //     output: `@Module({
-        //         imports: [
-        //             CoreModule,
-        //             DatabaseModule,
-        //             AuthzModule,
-        //             OpenTelemetryModule.forRoot({
-        //                 metrics: {
-        //                     hostMetrics: true,
-        //                     apiMetrics: {
-        //                         enable: true,
-        //                     },
-        //                 },
-        //             }),
-        //             UserInternalModule,
-        //         ],
-        //         controllers: [
-        //             AController,
-        //             BController
-        //         ],
-        //         providers: [
-        //             AProvider,
-        //             BProvider
-        //         ],
-        //     })
-        //     export class MainModule {}`,
-        //     errors: [
-        //         {
-        //             messageId: "moduleMetadataArraysAreSorted",
-        //         },
-        //     ],
-        // },
-        // {
-        //     code: `@Module({
-        //         imports: [
-        //             AuthzModule,
-        //             CoreModule,
-        //             DatabaseModule,
-        //             OpenTelemetryModule.forRoot({
-        //                 metrics: {
-        //                     hostMetrics: true,
-        //                     apiMetrics: {
-        //                         enable: true,
-        //                     },
-        //                 },
-        //             }),
-        //             UserInternalModule,
-        //         ],
-        //         controllers: [
-        //             BController,
-        //             AController
-        //         ],
-        //         providers: [
-        //             AProvider,
-        //             BProvider
-        //         ],
-        //     })
-        //     export class MainModule {}`,
-        //     output: `@Module({
-        //         imports: [
-        //             AuthzModule,
-        //             CoreModule,
-        //             DatabaseModule,
-        //             OpenTelemetryModule.forRoot({
-        //                 metrics: {
-        //                     hostMetrics: true,
-        //                     apiMetrics: {
-        //                         enable: true,
-        //                     },
-        //                 },
-        //             }),
-        //             UserInternalModule,
-        //         ],
-        //         controllers: [
-        //             AController,
-        //             BController
-        //         ],
-        //         providers: [
-        //             AProvider,
-        //             BProvider
-        //         ],
-        //     })
-        //     export class MainModule {}`,
-        //     errors: [
-        //         {
-        //             messageId: "moduleMetadataArraysAreSorted",
-        //         },
-        //     ],
-        // },
+        {
+            code: `@Module({
+                imports: [
+                    CoreModule,
+                    DatabaseModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    AuthzModule,
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+            output: [
+                `@Module({
+                imports: [
+                    CoreModule,
+                    DatabaseModule,
+                    AuthzModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+                `@Module({
+                imports: [
+                    CoreModule,
+                    AuthzModule,
+                    DatabaseModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+                `@Module({
+                imports: [
+                    AuthzModule,
+                    CoreModule,
+                    DatabaseModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+            ],
+            errors: [
+                {
+                    messageId: "moduleMetadataArraysAreSorted",
+                },
+            ],
+        },
+        {
+            code: `@Module({
+                imports: [
+                    AuthzModule,
+                    CoreModule,
+                    DatabaseModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    BController,
+                    AController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+            output: `@Module({
+                imports: [
+                    AuthzModule,
+                    CoreModule,
+                    DatabaseModule,
+                    OpenTelemetryModule.forRoot({
+                        metrics: {
+                            hostMetrics: true,
+                            apiMetrics: {
+                                enable: true,
+                            },
+                        },
+                    }),
+                    UserInternalModule,
+                ],
+                controllers: [
+                    AController,
+                    BController
+                ],
+                providers: [
+                    AProvider,
+                    BProvider
+                ],
+            })
+            export class MainModule {}`,
+            errors: [
+                {
+                    messageId: "moduleMetadataArraysAreSorted",
+                },
+            ],
+        },
     ],
 });
