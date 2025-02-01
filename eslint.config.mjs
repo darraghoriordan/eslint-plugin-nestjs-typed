bb0e86346f3e651459f4ceef1b80a522da6cdc81
Darragh ORiordan <darragh.oriordan@gmail.com>
2025-02-01 21:38:22 +1100

    feat: change to vitest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ export default tseslint.config(
             "dist/**",
             "**/commitlint.config.js",
             "**/eslint.config.mjs",
+            "**/vitest.config.ts",
         ],
     },
     eslint.configs.recommended,
